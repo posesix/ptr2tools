2a2c9fa975c1db7d2bc98c81f98b584a76afa18b
MGRich <nothing>
2018-02-25 16:39:01 -0600

    add note

--- sources/ptr2int/ptr2int.cpp
+++ sources/ptr2int/ptr2int.cpp
@@ -46,7 +46,8 @@ cmd_t commands[] = {
     {"c", "make", "pack"}, 3, cmd_create
   },
   { "optimize", "[folder] [out-neworderfile]",
-    "Attempts to optimize the _order.txt file of a folder to achieve lzss compression.",
+    "Attempts to optimize the _order.txt file of a folder to achieve lzss compression.
+	Rich's note: I RECOMMEND NOT USING THIS.",
     {"optimise"}, 1, cmd_optimize
   }
 };
